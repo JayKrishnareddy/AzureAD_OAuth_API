e5aa8d5bec462799fe343b98f92a63246629f09f
Jay Krishna Reddy <majji.jaya_krishna_reddy@nokia.com>
2021-05-09 23:01:33 +0530

    Swagger Configuration for Azure AD Auth

--- AzureAD_OAuth_API/Startup.cs
+++ AzureAD_OAuth_API/Startup.cs
@@ -36,7 +36,41 @@ namespace AzureAD_OAuth_API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AzureAD_OAuth_API", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows()
+                    {
+                        Implicit = new OpenApiOAuthFlow()
+                        {
+                            AuthorizationUrl = new Uri("https://login.microsoftonline.com/60879ec9-9cda-42a6-9611-4bc2ee2fcedc/oauth2/v2.0/authorize"),
+                            TokenUrl = new Uri("https://login.microsoftonline.com/60879ec9-9cda-42a6-9611-4bc2ee2fcedc/oauth2/v2.0/token"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "api://1642dc16-891c-4f3c-9769-6c7f38e2a989/ReadWriteAccess", "Reads the Weather forecast" }
+                            }
+                        }
+                    }
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                     {
+                     new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                        {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "oauth2"
+                        },
+                                Scheme = "oauth2",
+                                Name = "oauth2",
+                                In = ParameterLocation.Header
+                     },
+                        new List<string>()
+                     }
+                });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +80,15 @@ namespace AzureAD_OAuth_API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AzureAD_OAuth_API v1"));
+                app.UseSwaggerUI(c =>
+                {
+                   c.SwaggerEndpoint("/swagger/v1/swagger.json", "AzureAD_OAuth_API v1");
+                   //c.RoutePrefix = string.Empty;
+                   c.OAuthClientId("1642dc16-891c-4f3c-9769-6c7f38e2a989");
+                   c.OAuthClientSecret("Mp5YKhbwbd4hUh3dDk9Ca.61RTd_Sv~IT~");
+                   c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+                }
+                );
             }
 
             app.UseHttpsRedirection();
